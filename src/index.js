05d8f96a1f71f7dcef9079dca6b140a53d473717
Diego Gonzalez <degonzalez6@gmail.com>
2022-09-28 00:40:46 -0300

    Se cambió la contraseña por seguridad

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ const userRoutes = require("./routes/user");
 const app = express();
 const port = process.env.PORT || 9000;
 
-    //middleware
-    app.use(express.json());
-    app.use('/api', userRoutes);
+//middleware
+app.use(express.json());
+app.use('/api', userRoutes);
     
-
-
 //Routes
 app.get('/', (req, res) =>{
     res.send('Welcome');
@@ -19,7 +17,7 @@ app.get('/', (req, res) =>{
 
 //mongodb connection
 mongoose
-    .connect("mongodb+srv://diego:Noentrasma$@cluster0.x6d80.mongodb.net/?retryWrites=true&w=majority")
+    .connect("mongodb+srv://diego:<pass>@cluster0.x6d80.mongodb.net/?retryWrites=true&w=majority")
     .then(()=> console.log('Connected to Mongo Atlas'))
     .catch((error)=> console.error(error));
 
